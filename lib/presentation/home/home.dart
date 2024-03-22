d8f97edcb5045dde634893df0812aa02cad149d3
Alex Tymoshenko <oleksandr.tymoshenko@gostudent.org>
2024-03-22 14:38:43 +0100

    Removing import

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -7,7 +7,6 @@ import 'package:go_router/go_router.dart';
 import 'package:sport_timer/features/timer/timer.dart';
 import 'package:sport_timer/navigation.dart';
 import 'package:sport_timer/presentation/widgets/timer_settings_picker.dart';
-import 'package:sport_timer/presentation/widgets/stopwatch_widget.dart';
 import 'package:sport_timer/theme/styles/styles.dart';
 
 part 'home_page.dart';
